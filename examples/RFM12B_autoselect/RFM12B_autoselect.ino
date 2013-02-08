c4e76fc9ae88d491cd8cd224cfe781ed101448f4
Steve Marple <stevemarple@googlemail.com>
2013-02-08 00:43:01 +0000

    Using pointer to Stream so radio can be changed. Add some debug output.

--- examples/RFM12B_autoselect/RFM12B_autoselect.ino
+++ examples/RFM12B_autoselect/RFM12B_autoselect.ino
@@ -1,12 +1,10 @@
 /*
- * Example sketch of how the begin() function can be used to check
- * whether the RFM12B is fitted, and to fall back to using a XRF/XBee
- * radio if it is not. By using a reference to a Stream object this
- * concept can be used to easily create a single firmware image which
- * supports both types of hardware.
+ * Example sketch to demonstrate the concept of how a single firmware
+ * image can support the RFM12B and Ciseco XRF (or XBee) modules,
+ * autoselecting between them depending on the hardware fitted. This
+ * example assumes that the XRF is connected to Serial1, leaving
+ * Serial free for debugging information.
  *
- * Copyright S R Marple, 2013.
- * Released under MIT License, http://opensource.org/licenses/mit-license.php
  */
 
 
@@ -28,7 +26,7 @@ RF12_Stream rfm12b(rxBuffer, sizeof(rxBuffer), txBuffer, sizeof(txBuffer));
  * not exist (eg ATtiny). Use RF12_Stream::begin() in setup() to point
  * the reference to Serial if the RFM12B is not found.
  */
-Stream &radio = rfm12b;
+Stream *radioPtr;
 
 AsyncDelay txTimer;
 
@@ -39,12 +37,13 @@ void setup(void)
 #endif
   Serial.begin(9600);
   if (rfm12b.begin(rfm12bCs, rfm12bIrqPin, rfm12bIrqNum, 1, RF12_433MHZ)) {
-    // RFM12B exists, add any other RFM12B setup code here
-    ;
+    radioPtr = &rfm12b;
+    Serial.println("Found RFM12B");
   }
   else {
-    // No RFM12B, must use XRF
-    radio = Serial;
+    Serial.println("RFM12B not found");
+    Serial1.begin(9600);
+    radioPtr = &Serial1;
   }
 }
 
@@ -58,7 +57,8 @@ void loop(void)
 
   if (txTimer.isExpired()) {
     // Send message every second
-    radio.print("Send a message");
+    Serial.println("Sending message"); 
+    radioPtr->print("Send a message");
     txTimer.start(1000, AsyncDelay::MILLIS);
   }
 
